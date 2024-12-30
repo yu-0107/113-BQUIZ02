6ab42480c4989031bcc524bd019c883500d843ac
onion <a0966550619@gmail.com>
2024-12-30 10:26:09 +0800

    複製出一個admin.php檔案並修改對應的內容

--- admin.php
+++ admin.php
@@ -27,11 +27,11 @@
         </div>
         <div id="mm">
             <div class="hal" id="lef">
+                <a class="blo" href="?do=acc">帳號管理</a>
                 <a class="blo" href="?do=po">分類網誌</a>
-                <a class="blo" href="?do=news">最新文章</a>
-                <a class="blo" href="?do=pop">人氣文章</a>
-                <a class="blo" href="?do=know">講座訊息</a>
-                <a class="blo" href="?do=que">問卷調查</a>
+                <a class="blo" href="?do=news">最新文章管理</a>
+                <a class="blo" href="?do=know">講座管理</a>
+                <a class="blo" href="?do=que">問卷管理</a>
             </div>
             <div class="hal" id="main">
                 <div>
@@ -51,7 +51,7 @@
 					if(file_exists($file)){
 						include $file;
 					}else{
-						include "front/main.php";
+						include "back/main.php";
 					}
 				?>
             </div>
