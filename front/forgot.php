66acf5ba9fa83ec00c5d14e337fc5e7ad6abc03f
onion <a0966550619@gmail.com>
2024-12-30 15:11:26 +0800

    建立會員登入頁面

---
+++ front/forgot.php
@@ -0,0 +1,19 @@
+<fieldset style="width:50%;margin:auto;">
+    <legend>忘記密碼</legend>
+    <table style="width:98%;">
+        <tr>
+            <td>請輸入信箱以查詢密碼</td>
+        </tr>
+        <tr>
+            <td><input type="text" name="email" id="email" style="width:98%"></td>
+        </tr>
+        <tr>
+            <td id="result"></td>
+        </tr>
+        <tr>
+            <td>
+                <input type="button" value="尋找">
+            </td>
+        </tr>
+    </table>
+</fieldset>
