a75a9e3425bace3dc0700ea8f8c06ce89705cded
onion <a0966550619@gmail.com>
2024-12-30 09:30:41 +0800

    加入標題圖片及日期,並調整CSS

--- index.php
+++ index.php
@@ -19,9 +19,11 @@
     </div>
     <div id="all">
         <div id="title">
-            00 月 00 號 Tuesday | 今日瀏覽: 1 | 累積瀏覽: 36 </div>
+            <?=date("m 月 d 號 l");?> | 今日瀏覽: 1 | 累積瀏覽: 36 </div>
         <div id="title2">
-
+            <a href="index.php" title="健康促進網-回首頁">
+                <img src="icon/02B01.jpg" alt="健康促進網-回首頁">
+            </a>
         </div>
         <div id="mm">
             <div class="hal" id="lef">
@@ -52,7 +54,7 @@
             </div>
         </div>
         <div id="bottom">
-            本網站建議使用：IE9.0以上版本，1024 x 768 pixels 以上觀賞瀏覽 ， Copyright © 2012健康促進網社群平台 All Right Reserved
+            本網站建議使用：IE9.0以上版本，1024 x 768 pixels 以上觀賞瀏覽 ， Copyright © 2024健康促進網社群平台 All Right Reserved
             <br>
             服務信箱：health@test.labor.gov.tw<img src="./icon/02B02.jpg" width="45">
         </div>
