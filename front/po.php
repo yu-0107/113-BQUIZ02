9d98c2c994f32bdad73174677a1d19b56203ba9e
onion <a0966550619@gmail.com>
2025-01-03 09:02:31 +0800

    完成分類網誌頁面建置

--- front/po.php
+++ front/po.php
@@ -1,1 +1,20 @@
-分類網誌
+<div>
+    目前位置：首頁 > 分類網誌 <span id='type'>健康新知</span>
+</div>
+<style>
+.type {
+    display: block;
+    margin: 10px 0;
+}
+</style>
+<fieldset style="width: 150px;display:inline-block;vertical-align:top">
+    <legend>分類網誌</legend>
+    <a href="javascript:getList(1)" class='type'>健康新知</a>
+    <a href="javascript:getList(2)" class='type'>菸害防制</a>
+    <a href="javascript:getList(3)" class='type'>癌症防治</a>
+    <a href="javascript:getList(4)" class='type'>慢性病防治</a>
+</fieldset>
+
+<fieldset style='width: 500px;display:inline-block;'>
+    <legend>文章列表</legend>
+</fieldset>
