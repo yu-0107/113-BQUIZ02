dda80b6e9744e6c3d87ba42a645605e8be65b58c
onion <a0966550619@gmail.com>
2025-01-03 14:24:23 +0800

    完成人氣文章,按讚及彈出視窗

--- api/like.php
+++ api/like.php
@@ -3,10 +3,16 @@
 $id=$_POST['id'];
 $user=$_SESSION['user'];
 
+$news=$News->find($id);
+
 $chk=$Log->count(['news'=>$id,'user'=>$user]);
 
 if($chk>0){
     $Log->del(['news'=>$id,'user'=>$user]);
+    $news['likes']--;
 }else{
     $Log->save(['news'=>$id,'user'=>$user]);
+    $news['likes']++;
 }
+
+$News->save($news);
--- front/pop.php
+++ front/pop.php
@@ -40,6 +40,8 @@
                 </span>
             </td>
             <td>
+                <?=$row['likes'];?>個人說
+                <img src="./icon/02B03.jpg" style="width: 25px;">
                 <?php 
                 if(isset($_SESSION['user'])){
                     $chk=$Log->count(['news'=>$row['id'],'user'=>$_SESSION['user']]);
@@ -84,6 +86,7 @@ $(".like").on("click", function() {
                     $(this).text("讚");
                     break;
             }
+            location.reload();
         })
 })
 
